c1e1fcec38338f672a4881b9f7bd91537101917d
Amitabh-Phule <amitabhphule4@gmail.com>
2026-03-04 10:49:22 +0530

    exp1

---
+++ ArithmaticOp/ArithmaticOp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArithmeticOp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Addition: " + (a + b));
+            Console.WriteLine("Subtraction: " + (a - b));
+            Console.WriteLine("Multiplication: " + (a * b));
+
+            if (b != 0)
+            {
+                Console.WriteLine("Division: " + (a / b));
+                Console.WriteLine("Modulus: " + (a % b));
+            }
+            else
+            {
+                Console.WriteLine("Division and Modulus not possible (division by zero).");
+            }
+        }
+    }
+}
