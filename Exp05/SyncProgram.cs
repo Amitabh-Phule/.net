6fa08c7f81b80c5f9e7b3b12b19d58116f5d8f56
Amitabh-Phule <amitabhphule4@gmail.com>
2026-04-28 20:30:05 +0530

    Exp5

---
+++ Exp05/SyncProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Thinking.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Start");
+        await LongTask();
+        Console.WriteLIne("End");
+    }
+    {
+        Console.WriteLine("Long Task Started");
+        await Task.Delay(3000);
+        Console.WriteLine("Long Task Completed");
+
+    }
+}
---
+++ Exp05/SyncProgram/Async Await/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Start");
+
+        Console.WriteLine("Task Started...");
+        Thread.Sleep(3000); // Blocking
+        Console.WriteLine("Task Completed");
+
+        Console.WriteLine("End");
+    }
+}
